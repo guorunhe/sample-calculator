52410e199bd41cb8a05c02c1384d459621db5db1
guorunhe <guorunhe@baidu.com>
2024-04-17 13:28:43 +0800

    feature: 一个计算器

--- cn/ViewController.m
+++ cn/ViewController.m
@@ -8,6 +8,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *jisuan1;
+@property (weak, nonatomic) IBOutlet UITextField *jisuan2;
+@property (weak, nonatomic) IBOutlet UITextField *sum;
+- (IBAction)sumb:(id)sender;
 
 @end
 
@@ -18,5 +22,15 @@
     // Do any additional setup after loading the view.
 }
 
-
+- (IBAction)sumb:(id)sender {
+    NSString *jisuan1 = self.jisuan1.text;
+    NSString *jisuan2 = self.jisuan2.text;
+    int result = [jisuan1 intValue] + [jisuan2 intValue];
+    self.sum.text = [NSString stringWithFormat:@"%d", result];
+    [self.jisuan1 resignFirstResponder];
+    [self.jisuan2 resignFirstResponder];
+    [self.view endEditing:YES];
+}
+//- (IBAction)sumb:(id)sender {
+//}
 @end
